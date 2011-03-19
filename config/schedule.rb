d54276ae19c1c9ebba5fc1e1ce8603f5f7038f69
Ken Erickson <bookworm.productions@gmail.com>
2011-03-19 15:54:53 -0700

    updated wehenevr

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,6 +5,6 @@ every 2.hours do
   command "cd /home/ubuntu/notebook.designbreakdown.com/dbd_notebook && bundle exec padrino rake backup:runit trigger='mongodb-backup-s3' "
 end        
 
-every 25.minutes do       
-  command "cd /home/ubuntu/notebook.designbreakdown.com/dbd_notebook && bundle exec padrino rake navvy:work"
-end
+# every 25.minutes do       
+#   command "cd /home/ubuntu/notebook.designbreakdown.com/dbd_notebook && bundle exec padrino rake navvy:work"
+# end
